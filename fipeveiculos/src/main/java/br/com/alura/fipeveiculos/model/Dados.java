279aa17195b51a84408931ab15eb7b35d75f6edf
ricardo <ric.vecchio@gmail.com>
2024-02-14 23:06:14 -0300

    Construindo projeto da consulta a tabela Fipe.

--- fipeveiculos/src/main/java/br/com/alura/fipeveiculos/model/Dados.java
+++ fipeveiculos/src/main/java/br/com/alura/fipeveiculos/model/Dados.java
@@ -1,17 +1,9 @@
 package br.com.alura.fipeveiculos.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Dados(String codigo, String nome, List<DadosVeiculo> veiculos) {
+public record Dados(String codigo, String nome) {
 
-    @Override
-    public String toString() {
-        return "Cód: " + codigo +
-                ", Descrição: " + nome ;
-    }
 }
 
--- fipeveiculos/src/main/java/br/com/alura/fipeveiculos/principal/Principal.java
+++ fipeveiculos/src/main/java/br/com/alura/fipeveiculos/principal/Principal.java
@@ -1,7 +1,8 @@
 package br.com.alura.fipeveiculos.principal;
 
 import br.com.alura.fipeveiculos.model.Dados;
-import br.com.alura.fipeveiculos.model.DadosVeiculo;
+import br.com.alura.fipeveiculos.model.Modelos;
+import br.com.alura.fipeveiculos.model.Veiculo;
 import br.com.alura.fipeveiculos.service.ConsumoApi;
 import br.com.alura.fipeveiculos.service.ConverteDados;
 
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -18,12 +20,13 @@ public class Principal {
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
     public void exibeMenu() {
+
         var menu = """
                **** OPÇÕES ****
-               Carro 
+               Carro
                Moto
                Caminhão
-               
+
                Digite uma das opções para consultar valores: """;
         System.out.println(menu);
 
@@ -43,74 +46,78 @@ public class Principal {
         marcas.stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
-
-        System.out.println("Informe o código da marca para consulta:");
+/*
+        Recebe o código da marca digitado e efetua uma busca dos modelos de véiculos ordenando pelo código.
+*/
+        System.out.println("\nInforme o código da marca para consulta:");
         var codigoMarca = leitura.nextLine();
         endereco = endereco + codigoMarca + "/modelos";
         json = consumo.obterDados(endereco);
-
-        String jsonFormatado = json.replace("[", "").replace("]", "");
-        System.out.println(jsonFormatado);
-
-        //erro... modelos traz cabeçalho tbm junto
-        endereco = "https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos";
-        json = consumo.obterDados(endereco);
-
-        var modelos = conversor.obterLista(json, Dados.class);
-        modelos.stream()
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+        modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
-
-
-        System.out.println("Digite um trecho do nome do veículo para consulta:");
+/*
+        Efetua uma busca do veículo pelo trecho digitado e cria uma nova lista.
+*/
+        System.out.println("\nDigite um trecho do nome do veículo para consulta:");
         var nomeVeiculo = leitura.nextLine();
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+        System.out.println("\nModelos Filtrados");
+        modelosFiltrados.forEach(System.out::println);
+/*
+        Efetua uma busca da lista de anos disponíveis para o código do veiculo selecionado.
+*/
+        System.out.println("\nDigite o código do modelo para buscar os valores de avaliação:");
+        var codigoModelo = leitura.nextLine();
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumo.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+/*
+        Cria uma nova lista de veículos, incrementando com os dados dos anos disponíveis.
+*/
+        List<Veiculo> veiculos = new ArrayList<>();
+		for(int i = 0; i < anos.size(); i++){
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumo.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+            }
+        System.out.println("\nTodos os veículos filtrados com avaliações por ano: ");
+        veiculos.forEach(System.out::println);
 
-        System.out.println("Digite o código do modelo para consultar valores:");
-        var codVeiculo = leitura.nextLine();
-
-
-//      List<DadosVeiculo> veiculos1 = new ArrayList<>();
-//
-//		for(int i = 1; i <= json.length(); i++){
-//            var codigoMarca = leitura.nextLine();
-//            endereco = "https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/";
-//            endereco = endereco + codigoMarca + "/modelos";
-//            json = consumo.obterDados(endereco);
-//
-//            endereco = "https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/";
-//            json = consumo.obterDados(endereco);
-//            DadosVeiculo listaVeiculos = conversor.obterDados(json, DadosVeiculo.class);
-//            veiculos1.add(listaVeiculos);
-//		}
-//        veiculos1.forEach(System.out::println);
-
-
-//
-//      ANOTAÇÕES:
-//
-//      *** Buscando todos os titulos de cada episodio de todas as temporadas ***
-//
-//     **** FUNÇÃO SEM LAMBDA ****
-//
-//		for(int i = 0; i < dados.totalTemporadas(); i++){
-//          List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for(int j = 0; j < episodiosTemporada.size(); j++){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//		}
-//
-//      *** FUNÇÃO USANDO LAMBDA:
-//
-//      temporadas.forEach(t -> t.episodios().forEach(e ->  System.out.println(e.titulo);
-//
-//          Leitura do código abaixo:
-//          temporadas.forEach      ==> Ler todas as temporadas(coleção) forEach(médodo).
-//          (t -> t.episodios()     ==> Para cada temporada(t), percorrer a lista dos episódios.
-//          .forEach(e ->  System.out.println(e.titulo) ==> Ler todos os episódios e imprimir o título de cada um.
-//
-//
-//      *** Imprimindo todas as temporadas usando lambda (ambos estão fazendo a mesma coisa abaixo):
-//        temporadas.forEach(t -> System.out.println(t));
-//        temporadas.forEach(System.out::println);
     }
 }
+
+/*
+     ANOTAÇÕES:
+     ==========
+
+     *** Buscando todos os titulos de cada episodio de todas as temporadas ***
+
+     **** FUNÇÃO SEM LAMBDA ****
+
+		for(int i = 0; i < dados.totalTemporadas(); i++){
+          List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+		}
+
+      *** FUNÇÃO USANDO LAMBDA: (parametro) -> expressão ***
+
+      temporadas.forEach(t -> t.episodios().forEach(e ->  System.out.println(e.titulo);
+
+          Leitura do código abaixo:
+          temporadas.forEach      ==> Ler todas as temporadas(coleção) forEach(médodo).
+          (t -> t.episodios()     ==> Para cada temporada(t), percorrer a lista dos episódios.
+          .forEach(e ->  System.out.println(e.titulo) ==> Ler todos os episódios e imprimir o título de cada um.
+
+
+      *** Imprimindo todas as temporadas usando lambda (ambos estão fazendo a mesma coisa abaixo):
+        temporadas.forEach(t -> System.out.println(t));
+        temporadas.forEach(System.out::println);
+
+ */
